5ba19f2c92f26230df8eab2406fcb22e79641aad
Olli Brummer <olli.brummer@knowit.fi>
2021-11-15 15:57:20 +0200

    Added routes and types for patients and diagnoses

---
+++ src/services/patientService.ts
@@ -0,0 +1,27 @@
+import patients from "../../data/patients";
+
+import { PatientEntry, NonSensitivePatientEntry } from "../types";
+
+const getEntries = (): Array<PatientEntry> => {
+  return patients;
+};
+
+const getNonSensitiveEntries = (): NonSensitivePatientEntry[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
+const addEntry = () => {
+  return null;
+};
+
+export default {
+  getEntries,
+  addEntry,
+  getNonSensitiveEntries,
+};
